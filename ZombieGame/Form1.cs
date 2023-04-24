2a4d894182851c18fff2aee0f47b81caee6fb286
Eric <zverical06@protonmail.ch>
2023-04-24 11:42:58 +0100

    Add files via upload

---
+++ ZombieGame/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace ZombieGame
+{
+    public partial class MainGame : Form
+    {
+        ImageSprite player;
+        ImageSprite zombie;
+        public MainGame()
+        {
+            InitializeComponent();
+        }
+
+        private void MainGame_Load(object sender, EventArgs e)
+        {
+            PointF StartPosition = new PointF(100, 100);
+            player = new ImageSprite((0, StartPosition, Properties.Resources.hero));
+            StartPosition = new PointF(600, 300);
+            zombie = new ImageSprite(0, StartPosition, Properties.Resources.zombie);
+
+        }
+    }
+}
---
+++ ZombieGame/ImageSprite.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+
+namespace ZombieGame
+{
+    class ImageSprite
+    {
+        public float Angle;
+        public PointF Center;
+        public Bitmap Picture;
+        public float Radius;
+
+        public ImageSprite(float angle, PointF center, Bitmap picture)
+        {
+            Angle = angle;
+            Center = center;
+            Picture = picture;
+            Radius = Math.Min(picture.Width, picture.Height) / 2f;
+        }
+
+        public void Draw(Graphics gr)
+        {
+            GraphicsState state = gr.Save();
+            gr.ResetTransform();
+            gr.RotateTransform(Angle);
+            gr.TranslateTransform(Center.X, Center.Y, MatrixOrder.Append);
+            gr.DrawImage(Picture, new PointF(-Radius, -Radius));
+            gr.Restore(state);
+
+        }
+    }
+}
